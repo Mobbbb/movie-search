a1782de87f42d1803ec5ee821742742d3af24ef0
chenyinke <491450147@qq.com>
2021-03-08 16:28:23 +0800

    init project

---
+++ src/Views/Index.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="Index">
+        <div class="hearder-wrap">
+            场景：<Input v-model="sceneText" placeholder="Enter scene..." class="header-input" />
+            动作：<Input v-model="actionText" placeholder="Enter action..." class="header-input" />
+            物体：<Input v-model="objectText" placeholder="Enter object..." class="header-input" />
+            <Button class="header-button" @click="clear">清空</Button>
+            <Button type="success" @click="search">搜索</Button>
+        </div>
+        <SearchList :params="searchList"/>
+        <Page :total="searchList.length" show-elevator  v-if="searchList.length"/>
+    </div>
+</template>
+
+<script>
+import SearchList from '@/components/SearchList.vue';
+
+export default {
+    name: 'Index',
+    components: {
+        SearchList,
+    },
+    data() {
+        return {
+            sceneText: '',
+            actionText: '',
+            objectText: '',
+            searchList: [],
+        };
+    },
+    methods: {
+        clear() {
+             this.searchList = [];
+        },
+        search() {
+            this.searchList = [1,2,2,2,2,2,2,2,2,2,2,2];
+        },
+    },
+}
+</script>
+
+<style scoped>
+    .Index{
+        padding: 48px 68px 24px 68px;
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        box-sizing: border-box;
+    }
+    .hearder-wrap{
+        white-space: nowrap;
+        flex-shrink: 0;
+    }
+    .header-input{
+        margin-right: 24px;
+        width: 200px;
+    }
+    .header-button{
+        margin: 0 24px 0 168px;
+    }
+</style>
---
+++ src/components/SearchList.vue
@@ -0,0 +1,55 @@
+<template>
+    <Card class="search-list-wrap">
+        <div class="empty-text" v-if="!params.length">请输入查询条件！</div>
+        <div class="search-list" v-else>
+            <div class="search-item" v-for="(item, index) in params" :key="`${index}1`">
+                <img src="//www.baidu.com/img/flexible/logo/pc/result.png" class="image-preview">
+                <div>电影名称</div>
+                <div>起止时间</div>
+            </div>
+        </div>
+    </Card>
+</template>
+
+<script>
+export default {
+    name: 'search-list',
+    props: ['params'],
+}
+</script>
+
+<style scoped>
+    .search-list-wrap{
+        margin: 24px 0;
+        flex: 1;
+        overflow-y: scroll;
+        overflow-x: hidden;
+    }
+    .empty-text{
+        text-align: center;
+        font-size: 18px;
+        margin-top: 208px;
+    }
+    .search-list{
+        width: 100%;
+        min-width: 1101px;
+        height: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+    .search-item{
+        text-align: center;
+        margin: 0 48px 24px 0;
+    }
+    .image-preview{
+        width: 307px;
+        height: 128px;
+    }
+</style>
+
+<style>
+    .search-list-wrap .ivu-card-body{
+        height: 100%;
+    }
+</style>
