3d7f6cce4720ed84f318718b0e4e3d7418a73a37
Mobbbb <mobbbb.top@gmail.com>
2021-03-09 18:39:14 +0800

    数据mock

--- src/Views/Index.vue
+++ src/Views/Index.vue
@@ -1,19 +1,32 @@
 <template>
     <div class="Index">
-        <div class="hearder-wrap">
+        <div class="header-wrap">
             场景：<Input v-model="sceneText" placeholder="Enter scene..." class="header-input" />
             动作：<Input v-model="actionText" placeholder="Enter action..." class="header-input" />
             物体：<Input v-model="objectText" placeholder="Enter object..." class="header-input" />
             <Button class="header-button" @click="clear">清空</Button>
             <Button type="success" @click="search">搜索</Button>
         </div>
-        <SearchList :params="searchList"/>
-        <Page :total="searchList.length" show-elevator  v-if="searchList.length"/>
+        <SearchList :params="showSearchList" :isSearchResult="isSearchResult" />
+        <Page :page-size="pageSize" :total="total" show-elevator v-if="total" @on-change="changePage" />
     </div>
 </template>
 
 <script>
 import SearchList from '@/components/SearchList.vue';
+
+const movieName1 = 1291543;
+const movieName2 = 1291544;
+const sceneData = {}
+const actionData = {};
+const objectData = {};
+
+sceneData[movieName1] = require('@/mock/场景/' + movieName1 + '.json').shot;
+sceneData[movieName2] = require('@/mock/场景/' + movieName2 + '.json').shot;
+actionData[movieName1] = require('@/mock/动作/' + movieName1 + '.json').shot;
+actionData[movieName2] = require('@/mock/动作/' + movieName2 + '.json').shot;
+objectData[movieName1] = require('@/mock/物体/' + movieName1 + '.json').shot;
+objectData[movieName2] = require('@/mock/物体/' + movieName2 + '.json').shot;
 
 export default {
     name: 'Index',
@@ -22,18 +35,97 @@ export default {
     },
     data() {
         return {
+            page: 1,
+            pageSize: 12,
             sceneText: '',
             actionText: '',
             objectText: '',
-            searchList: [],
+            allSearchList: [], // 筛选出的符合条件的列表集合
+            isSearchResult: false, // 此次搜索输入框是否有内容输入
         };
     },
-    methods: {
-        clear() {
-             this.searchList = [];
+    computed: {
+        showSearchList() { // 当前页展示的列表集合
+            if (this.allSearchList.length && this.searchAble) {
+                let resultList = this.allSearchList.slice((this.page - 1) * this.pageSize, this.page * this.pageSize);
+                const restNum = this.pageSize - resultList.length;
+                if (restNum > 0) {
+                    resultList = resultList.concat(Array.from({ length: restNum }, () => 0))
+                }
+                return resultList;
+            }
+            return [];
         },
+        searchAble() {
+            return this.sceneText || this.actionText || this.objectText;
+        },
+        total() { // 总条数
+            return this.allSearchList.length;
+        },
+    },
+    methods: {
+        /**
+         * @description 翻页
+         */
+        changePage(page) {
+            this.page = page;
+        },
+        /**
+         * @description 清空
+         */
+        clear() {
+            this.sceneText = '';
+            this.actionText = '';
+            this.objectText = '';
+            this.allSearchList = [];
+            this.isSearchResult = false;
+        },
+        /**
+         * @description 搜索
+         */
         search() {
-            this.searchList = [1,2,2,2,2,2,2,2,2,2,2,2];
+            this.page = 1;
+            this.allSearchList = [];
+            this.isSearchResult = this.sceneText || this.actionText || this.objectText;
+            this.getAllSearchListByIndex(sceneData, this.sceneText);
+            this.getAllSearchListByIndex(actionData, this.actionText);
+            this.getAllSearchListByIndex(objectData, this.objectText);
+        },
+        getAllSearchListByIndex(jsonData, searchIndex) {
+            if (searchIndex) {
+                let key, shotItem, searchKey;
+                Object.keys(jsonData).forEach(movieName => {
+                    jsonData[movieName].forEach(item => {
+                        key = Object.keys(item)[0];
+                        shotItem = item[key];
+                        searchKey = JSON.stringify(shotItem[2]);
+                        if (searchKey.indexOf(searchIndex) > -1) {
+                            // 构造待展示的列表数据
+                            this.allSearchList.push({
+                                movieName,
+                                image: `http://${location.hostname}:8088/${movieName}/shot_${key}_img_1.jpg`, // 图片地址
+                                startTime: this.formatTime(shotItem[0]), // 开始时间
+                                endTime: this.formatTime(shotItem[1]), // 结束时间
+                            });
+                        }
+                    });
+                });
+            }
+        },
+        /**
+         * @description 时间格式化
+         * @param time
+         * @returns {string}
+         */
+        formatTime(time) {
+            let secondsNum = Number(time).toFixed(0);
+            let minutes = Math.floor(secondsNum / 60);
+            let seconds = secondsNum % 60;
+
+            minutes = minutes < 10 ? `0${minutes}` : minutes;
+            seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+            return `${minutes}:${seconds}`;
         },
     },
 }
@@ -41,13 +133,15 @@ export default {
 
 <style scoped>
     .Index{
-        padding: 48px 68px 24px 68px;
+        padding: 48px 68px 68px 68px;
         display: flex;
         flex-direction: column;
         height: 100%;
         box-sizing: border-box;
+        color: #262626;
+        font-size: 14px;
     }
-    .hearder-wrap{
+    .header-wrap{
         white-space: nowrap;
         flex-shrink: 0;
     }
--- src/components/SearchList.vue
+++ src/components/SearchList.vue
@@ -1,11 +1,16 @@
 <template>
     <Card class="search-list-wrap">
-        <div class="empty-text" v-if="!params.length">请输入查询条件！</div>
+        <div class="empty-text" v-if="!params.length">
+            <span v-if="isSearchResult">暂无数据！</span>
+            <span v-else>请输入查询条件！</span>
+        </div>
         <div class="search-list" v-else>
-            <div class="search-item" v-for="(item, index) in params" :key="`${index}1`">
-                <img src="//www.baidu.com/img/flexible/logo/pc/result.png" class="image-preview">
-                <div>电影名称</div>
-                <div>起止时间</div>
+            <div class="search-item-wrap" v-for="(item, index) in params" :key="`${index}1`">
+                <div class="search-item" v-if="item">
+                    <img :src="item.image" class="preview-image">
+                    <div class="item-title">电影名称：{{item.movieName}}</div>
+                    <div class="item-time">起止时间: {{item.startTime}} - {{item.endTime}}</div>
+                </div>
             </div>
         </div>
     </Card>
@@ -14,7 +19,10 @@
 <script>
 export default {
     name: 'search-list',
-    props: ['params'],
+    props: ['params', 'isSearchResult'],
+    mounted() {
+
+    },
 }
 </script>
 
@@ -22,29 +30,49 @@ export default {
     .search-list-wrap{
         margin: 24px 0;
         flex: 1;
-        overflow-y: scroll;
+        overflow-y: auto;
         overflow-x: hidden;
     }
     .empty-text{
-        text-align: center;
         font-size: 18px;
-        margin-top: 208px;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #595959;
     }
     .search-list{
         width: 100%;
         min-width: 1101px;
-        height: 100%;
         display: flex;
         flex-wrap: wrap;
         justify-content: space-between;
     }
-    .search-item{
-        text-align: center;
-        margin: 0 48px 24px 0;
+    .search-item-wrap{
+        width: 307px;
+        text-align: left;
+        margin: 0 24px 24px 24px;
     }
-    .image-preview{
+    .search-item{
+        
+    }
+    .search-item:hover{
+        cursor: pointer;
+    }
+    .search-item:hover .item-time,.search-item:hover .item-title{
+        color: #00a1d6;
+    }
+    .preview-image{
         width: 307px;
         height: 128px;
+    }
+    .item-title{
+        transition: color .3s;
+    }
+    .item-time{
+        color: #999;
+        font-size: 12px;
+        transition: color .3s;
     }
 </style>
 
